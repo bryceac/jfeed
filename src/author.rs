5f78196e33e3cdc65610870aa23ab140850e3d16
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-28 13:18:42 -0700

    serialize author values only when necessary.

--- src/author.rs
+++ src/author.rs
@@ -3,7 +3,10 @@ use url::Url;
 
 #[derive(Serialize, Clone, Debug)]
 pub struct Author {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub url: Option<Url>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub avatar: Option<Url>
 }
