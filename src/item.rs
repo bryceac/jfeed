cd240d3233fd0cb993041955f4423a76fb6c9135
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-28 16:22:51 -0700

    implement way to check is content is empty.

--- src/item.rs
+++ src/item.rs
@@ -8,17 +8,27 @@ use crate::Author;
 pub struct Item {
     id: String,
     url: Url,
+    #[serde(skip_serializing_if = "Option::is_none")]
     external_url: Option<Url>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     title: Option<String>,
     #[serde(flatten)]
-    content: Option<Content>,
+    content: Content,
+    #[serde(skip_serializing_if = "Option::is_none")]
     summary: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     image: Option<Url>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     banner: Option<Url>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     published: Option<DateTime<Utc>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     modified: Option<DateTime<Utc>>,
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
     authors: Vec<Author>,
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
     tags: Vec<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     language: Option<String>
 }
 
@@ -28,4 +38,10 @@ pub struct Content {
     pub html: Option<String>,
     #[serde(rename = "content_text", skip_serializing_if ="Option::is_none")]
     pub text: Option<String>
+}
+
+impl Content {
+    pub fn is_empty(&self) -> bool {
+        self.html.is_none() && self.text.is_none()
+    }
 }
