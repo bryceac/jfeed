6377df6257a292626a4ff07b2408888d6987cb86
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-28 16:40:36 -0700

    keep working on item.

--- src/item.rs
+++ src/item.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use chrono::prelude::*;
 use serde::{ Serialize, Deserialize };
 use url::Url;
@@ -38,4 +40,44 @@ pub struct Content {
     pub html: Option<String>,
     #[serde(rename = "content_text", skip_serializing_if ="Option::is_none")]
     pub text: Option<String>
+}
+
+#[derive(Default)]
+pub struct ContentBuilder {
+    html: Option<String>,
+    text: Option<String>
+}
+
+impl ContentBuilder {
+    pub fn set_html(&mut self,html: &str) -> &mut Self {
+        self.html = Some(html.to_owned());
+        self
+    }
+
+    pub fn set_text(&mut self, text: &str) -> &mut Self {
+        self.text = Some(text.to_owned());
+        self
+    }
+
+    pub fn build(&self) -> Content {
+        Content { html: (), text: () }
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(transparent)]
+struct ContentDes(HashMap<String, String>);
+
+impl TryFrom<ContentDes> for Content {
+    type Error = &'static str;
+
+    fn try_from(mut value: AuthorDes) -> Result<Self, Self::Error> {
+        if value.0.is_empty() {
+            return Err("Content cannot be left empty");
+        }
+
+        match value.0.keys {
+
+        }
+    }
 }
