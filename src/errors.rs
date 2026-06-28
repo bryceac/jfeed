8b6317980da77ad1273416485a450f6de3a63503
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-28 11:09:26 -0700

    add error for when both errors occur.

--- src/errors.rs
+++ src/errors.rs
@@ -3,14 +3,16 @@ use std::{ error::Error, fmt };
 #[derive(Debug)]
 pub enum AttachmentBuildError {
     URLNotFound,
-    MimeTypeNotFound
+    MimetypeNotFound,
+    URLAndMimetypeNotFound
 }
 
 impl fmt::Display for AttachmentBuildError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> fmt::Result {
         match self {
-            Self::MimeTypeNotFound => write!(f, "mimetype must be specified."),
-            Self::URLNotFound => write!(f, "URL must be specified.")
+            Self::MimetypeNotFound => write!(f, "mimetype must be specified."),
+            Self::URLNotFound => write!(f, "URL must be specified."),
+            Self::URLAndMimetypeNotFound => write!(f, "Both URL and mimetype must be specified")
         }
     }
 }
