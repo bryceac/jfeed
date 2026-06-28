6fc09a73e47141480f2d0e6bd583c3384d911717
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-28 09:53:53 -0700

    add a note regarding what the type is for.

---
+++ src/attachment.rs
@@ -0,0 +1,11 @@
+use serde::{ Serialize, Deserialize };
+
+/**
+ * JSON Feed attachment, 
+ * which is typically used for delivering podcasts.
+ * 
+ * For more information, refer to the following page for more details.
+ * 
+ * https://www.jsonfeed.org/version/1.1/index.html#attachments
+ */
+pub struct Attachment
