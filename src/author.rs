845fd3e0023e619568b0c53861c34c0971a76b8f
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-28 15:20:32 -0700

    fix calls to data.

--- src/author.rs
+++ src/author.rs
@@ -38,13 +38,13 @@ impl TryFrom<AuthorDes> for Author {
 
         for key in value.0.keys() {
             match key.clone() {
-                s if s == "name" => if let Some(name) = value.0.remove(key) {
+                s if s == "name" => if let Some(name) = value.0.get(key) {
                     builder.set_name(&name);
                 },
-                s if s == "url" => if let Some(url) = value.0.remove(key) {
+                s if s == "url" => if let Some(url) = value.0.get(key) {
                     builder.set_url(&url);
                 },
-                s if s == "avatar" => if let Some(avatar_url) = value.0.remove(key) {
+                s if s == "avatar" => if let Some(avatar_url) = value.0.get(key) {
                     builder.set_avatar(&avatar_url);
                 },
                 _ => {}
