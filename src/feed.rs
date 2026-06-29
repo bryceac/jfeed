a5c5dc9570cfe956d1f176cd5c6b581a9d33b623
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-29 10:08:37 -0700

    fix derive in feed.

--- src/feed.rs
+++ src/feed.rs
@@ -1,12 +1,12 @@
 use serde::{ Serialize, Deserialize };
 use url::Url;
 
-#[deive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum Version {
     JSONFeed1_1
 }
 
-#[deive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Feed {
 
 }
