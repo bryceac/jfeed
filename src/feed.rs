dbf07ae31fc97afa67cd69a3a755050fe7d52828
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-29 10:17:03 -0700

    create enum to handle feed versions.

--- src/feed.rs
+++ src/feed.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use serde::{ Serialize, Deserialize };
 use url::Url;
 
@@ -6,7 +8,25 @@ pub enum Version {
     JSONFeed1_1
 }
 
+impl ToString for Version {
+    fn to_string(&self) -> String {
+        match self {
+            Self::JSONFeed1_1 => "https://jsonfeed.org/version/1.1".to_owned()
+        }
+    }
+}
+
+impl FromStr for Version {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "https://jsonfeed.org/version/1.1" => Ok(Self::JSONFeed1_1),
+            _ => Err("invalid version.")
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Feed {
-
+    pub version: Version
 }
