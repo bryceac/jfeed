b6927097f63e1dc0da6b557cfaa760f48e61182f
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-28 17:31:19 -0700

    make sure dates retrieve the correct keys.

--- src/dates.rs
+++ src/dates.rs
@@ -3,8 +3,8 @@ use serde::{ Serialize, Deserialize };
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Dates {
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "date_published", skip_serializing_if = "Option::is_none")]
     published: Option<DateTime<Utc>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "date_modified", skip_serializing_if = "Option::is_none")]
     modified: Option<DateTime<Utc>>,
 }
