f1cdb976321932ed60c2083fb345668fe325005c
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-29 09:12:03 -0700

    make sure item fails without URL.

--- src/lib.rs
+++ src/lib.rs
@@ -252,4 +252,29 @@ mod tests {
 
         assert_eq!(format!("{}", builder.build().err().unwrap()), format!("{}", ItemBuildError::IDNotFound))
     }
+
+    #[test]
+    fn item_build_fails_without_url() {
+        let dates = Dates::builder()
+        .set_published("2026-06-28T08:55:00Z")
+        .build().unwrap();
+
+        let author = Author::builder()
+        .set_name("Jerry")
+        .build().unwrap();
+
+        let content = Content::builder()
+        .set_text("Hello, World!")
+        .build().unwrap();
+
+        let mut builder = Item::builder();
+        builder.set_id("https://example.com/hello_world.html");
+        builder.set_title("Hello, World!");
+        builder.set_dates(&dates);
+        builder.add_author(&author);
+        builder.set_content(&content);
+
+        assert_eq!(format!("{}", builder.build().err().unwrap()), format!("{}", ItemBuildError::URLNotFound))
+    }
+
 }
