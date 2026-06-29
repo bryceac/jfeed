c89affef0f9835729b849cfe236c259e897f082c
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-28 17:34:41 -0700

    create a builder for dates.

--- src/dates.rs
+++ src/dates.rs
@@ -4,7 +4,13 @@ use serde::{ Serialize, Deserialize };
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Dates {
     #[serde(rename = "date_published", skip_serializing_if = "Option::is_none")]
-    published: Option<DateTime<Utc>>,
+    pub published: Option<DateTime<Utc>>,
     #[serde(rename = "date_modified", skip_serializing_if = "Option::is_none")]
-    modified: Option<DateTime<Utc>>,
+    pub modified: Option<DateTime<Utc>>,
+}
+
+#[derive(Default)]
+pub struct DatesBuilder {
+    published: Option<String>,
+    modified: Option<String>
 }
