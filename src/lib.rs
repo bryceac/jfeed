3a688a3365f061af379c36804471c91767bf91e3
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-30 16:29:28 -0700

    move colons in JSON test.

--- src/lib.rs
+++ src/lib.rs
@@ -875,19 +875,19 @@ mod tests {
 
         let feed = "
         {
-            \"version:\" \"https://jsonfeed.org/version/1.1\",
-            \"title:\" \"News\",
-            \"homepage:\" \"https://example.com/\",
-            \"feed_url:\" \"https://example.com/feed.json\",
-            \"items:\" [
+            \"version\": \"https://jsonfeed.org/version/1.1\",
+            \"title\": \"News\",
+            \"homepage\": \"https://example.com/\",
+            \"feed_url\": \"https://example.com/feed.json\",
+            \"items\": [
                 {
-                    \"id:\" \"https://example.com/hello_world.html\",
-                    \"content_text:\" \"Hello, World!\",
-                    \"url:\" \"https://example.com/hello_world.html\",
-                    \"date_published:\" \"2026-06-28T08:55:00Z\",
-                    \"authors:\" [
+                    \"id\": \"https://example.com/hello_world.html\",
+                    \"content_text\": \"Hello, World!\",
+                    \"url\": \"https://example.com/hello_world.html\",
+                    \"date_published\": \"2026-06-28T08:55:00Z\",
+                    \"authors\": [
                         {
-                            \"name:\" \"Jerry\"
+                            \"name\": \"Jerry\"
                         }
                     ]
                 }
