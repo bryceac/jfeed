e1f53fa585b2ffd3cbcd90cff1bd21ecd0f58d95
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-30 08:13:50 -0700

    create initializer for hub.

--- src/hub.rs
+++ src/hub.rs
@@ -1,6 +1,8 @@
 use serde::{ Deserialize, Serialize };
 use url::Url;
 
+use crate::HubError;
+
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Hub {
     #[serde(rename = "type")]
@@ -9,5 +11,18 @@ pub struct Hub {
 }
 
 impl Hub {
-    pub fn from(hub_type: &str, url: &str)
+    pub fn from(hub_type: &str, url: &str) -> Result<Self, HubError> {
+        match (hub_type, url) {
+            (t, u) if t.is_empty() && u.is_empty() => Err(HubError::MissingAll),
+            (t, u) if !t.is_empty() && u.is_empty() => Err(HubError::NoURL),
+            (t, u) if t.is_empty() && !u.is_empty() => Err(HubError::NoType),
+            _ => match Url::parse(url) {
+                Ok(parsed_url) => Ok(Hub {
+                    hub_type: hub_type.to_owned(),
+                    url: parsed_url
+                }),
+                Err(error) => Err(HubError::URLError(error))
+            }
+        }
+    }
 }
