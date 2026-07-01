9d0768281442e80432fabedebc0331544f741906
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-30 17:49:04 -0700

    add comment to version.

--- src/version.rs
+++ src/version.rs
@@ -1,11 +1,13 @@
 use std::str::FromStr;
 
+/// Version of JSON feed being targetted
 #[derive(Clone, Debug)]
 pub enum Version {
     JSONFeed1_1
 }
 
 impl ToString for Version {
+    /// returns a URL string for the specified JSON feed version.
     fn to_string(&self) -> String {
         match self {
             Self::JSONFeed1_1 => "https://jsonfeed.org/version/1.1".to_owned()
@@ -15,6 +17,8 @@ impl ToString for Version {
 
 impl FromStr for Version {
     type Err = &'static str;
+
+    /// takes a URL string and returns a version.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "https://jsonfeed.org/version/1.1" => Ok(Self::JSONFeed1_1),
