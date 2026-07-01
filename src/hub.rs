8cc040f7e695326079a13f16b470859e701dda55
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-30 17:43:06 -0700

    add comments on hub.

--- src/hub.rs
+++ src/hub.rs
@@ -3,6 +3,11 @@ use url::Url;
 
 use crate::HubError;
 
+/**
+ * A Hub, as described at the address below.
+ * 
+ * https://www.jsonfeed.org/version/1.1/index.html#top-level
+ */
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Hub {
     #[serde(rename = "type")]
@@ -11,6 +16,14 @@ pub struct Hub {
 }
 
 impl Hub {
+    /**
+     * create a hub.
+     * 
+     * As the spec says that both fields are required,
+     * this function will fail unless both values are provided.
+     * 
+     * Also, it can fail if the provided URL cannot be parsed.
+     */
     pub fn from(hub_type: &str, url: &str) -> Result<Self, HubError> {
         match (hub_type, url) {
             (t, u) if t.is_empty() && u.is_empty() => Err(HubError::MissingAll),
