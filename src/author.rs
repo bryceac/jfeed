4f52d183e26487b3bc01285f37deb4eee03182df
Bryce Campbell <tonyhawk2100@gmail.com>
2026-07-02 10:38:06 -0700

    start working on fixing author.

--- src/author.rs
+++ src/author.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use serde::{ Serialize, Deserialize };
-use url::Url;
+use http::uri::Uri;
 
 use crate::AuthorBuildError;
 
@@ -15,10 +15,10 @@ use crate::AuthorBuildError;
 pub struct Author {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub url: Option<Url>,
-    #[serde(rename = "avatar", skip_serializing_if = "Option::is_none")]
-    pub avatar_url: Option<Url>
+    #[serde(skip_serializing_if = "Option::is_none", with = "http_serde::uri")]
+    pub url: Option<Uri>,
+    #[serde(rename = "avatar", skip_serializing_if = "Option::is_none", with = "http_serde::uri")]
+    pub avatar_url: Option<Uri>
 }
 
 impl Author {
